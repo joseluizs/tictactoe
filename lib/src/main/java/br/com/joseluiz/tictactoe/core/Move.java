fe54ffa0beb14b62d89a88351f4ac0fa1dc5da98
joseluizs <joseluiz.smarques@gmail.com>
2022-10-25 16:44:59 -0300

    Aplicando o encapsulamento

--- lib/src/main/java/br/com/joseluiz/tictactoe/core/Move.java
+++ lib/src/main/java/br/com/joseluiz/tictactoe/core/Move.java
@@ -1,7 +1,24 @@
 package br.com.joseluiz.tictactoe.core;
 
 public class Move {
-	
-	int i, j;
+
+	private int i;
+	private int j;
+
+	public int getI() {
+		return i;
+	}
+
+	public void setI(int i) {
+		this.i = i;
+	}
+
+	public int getJ() {
+		return j;
+	}
+
+	public void setJ(int j) {
+		this.j = j;
+	}
 
 }
