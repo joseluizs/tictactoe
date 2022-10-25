e6939143bfbbd98cfe5f0fc1769eea61cd1d8c9e
joseluizs <joseluiz.smarques@gmail.com>
2022-10-25 19:26:36 -0300

    Criando os jogadores do jogo

--- lib/src/main/java/br/com/joseluiz/tictactoe/core/Game.java
+++ lib/src/main/java/br/com/joseluiz/tictactoe/core/Game.java
@@ -10,10 +10,20 @@ public class Game {
 	
 	public void play() {
 		UI.printGameTitle();
-//		UI.readInput("Nome do Jogador: ");
 		
-		board.clear();
-		board.print();
+		for (int i = 0; i < players.length; i++) {
+			players[i] = createPlayer(i);
+		}
+	}
+	
+	private Player createPlayer(int index) {
+		String name = UI.readInput("Jogador " + (index + 1) + "=> ");
+		char symbol = Constants.SYMBOL_PLAYERS[index];
+		Player player = new Player(name, board, symbol);
+		
+		UI.printText("O jogador '" + name + "' vai usar o símbolo '" + symbol + "'");
+		return player;
+		
 	}
 
 }
